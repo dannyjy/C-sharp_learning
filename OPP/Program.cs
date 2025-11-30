595dd73e80eece9db3b276a09daeb1c45f32933e
dannyjy <danieljohn9295@gmail.com>
2025-11-30 23:30:46 +0200

    chore: add opp file configuration

---
+++ OPP/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+using OOP;
+
+namespace OPP
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Person p1 = new Person("John",34,"male");
+
+            JsonSerializer.Serialize(p1);
+            string fileName = "Users.json";
+
+            // File file = new File();
+        }
+    }
+}
