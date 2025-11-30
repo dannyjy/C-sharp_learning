f48a54d9e3e03e393adaa340eaae1c1c968bf106
dannyjy <danieljohn9295@gmail.com>
2025-11-30 23:30:09 +0200

    chore: add first.cs with the person class for learning classes.

---
+++ OPP/class/first.cs
@@ -0,0 +1,19 @@
+
+using System.Security.Cryptography;
+
+namespace OOP
+{
+    class Person
+    {
+        string? name;
+        int age;
+        string? gender;
+        
+        public Person(string? Name,int Age,string? Gender)
+        {
+            Name = name;
+            Age = age;
+            Gender = gender;
+        }
+    }
+}
