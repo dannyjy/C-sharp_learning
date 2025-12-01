7a21c5df7e26fc38584505e0103c7c79efd5f1bf
dannyjy <danieljohn9295@gmail.com>
2025-12-01 23:25:08 +0200

    refactor: modify file name

---
+++ OPP/class/Person.cs
@@ -0,0 +1,19 @@
+
+using System.Security.Cryptography;
+
+namespace OOP
+{
+    class Person
+    {
+        public string? name;
+        public int age;
+        public string? gender;
+        
+        public Person(string? Name,int Age,string? Gender)
+        {
+            Name = name;
+            Age = age;
+            Gender = gender;
+        }
+    }
+}
