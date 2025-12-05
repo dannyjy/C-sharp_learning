114bb8d3026cb226e35b60b96c664995bf537b28
dannyjy <danieljohn9295@gmail.com>
2025-12-05 23:56:28 +0200

    feat: add instance of Doctor class

--- OPP/Program.cs
+++ OPP/Program.cs
@@ -1,18 +1,26 @@
-﻿using System.Text.Json;
-using OOP;
-
-namespace OPP
+﻿namespace OOP
 {
     class Program
     {
         static void Main(String[] args)
         {
+
+            Car toyota = new Car();
+            toyota.year = 1995;
+            toyota.color = "red";
+            toyota.model = "GTRX10234";
+            toyota.name = "Toyota Prado G6";
+            // toyota.getCardDetails();
+
+            // Using constructors
             Person p1 = new Person("John",34,"male");
+            string? name = p1.name;
+            // Console.WriteLine(name);
 
-            JsonSerializer.Serialize(p1);
-            string fileName = "Users.json";
 
-            // File file = new File();
+            Doctor neurologist = new Doctor("John Doe",34,"Male","03324","Brain Surgen");
+            string detail = neurologist.GetDetails();
+            Console.WriteLine(detail);
         }
     }
 }
