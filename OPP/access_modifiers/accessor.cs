99f267d8f7265cda568e27e7305a9b45bf0b583b
dannyjy <danieljohn9295@gmail.com>
2025-12-22 20:00:21 +0200

    feat: add the access modifiers class

---
+++ OPP/access_modifiers/accessor.cs
@@ -0,0 +1,14 @@
+namespace OOP;
+
+public class Accessors
+{
+    public string? name;
+    private int accountNumber;
+    internal string? phoneNumber;
+    protected int age;
+}
+
+public class BankDetails : Accessors
+{
+    
+}
