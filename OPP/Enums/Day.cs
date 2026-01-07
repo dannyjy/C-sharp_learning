d0ec954549f16bb84083b7330136b4016bc6036a
dannyjy <danieljohn9295@gmail.com>
2026-01-07 21:42:46 +0200

    feat: add the an enum with a days of the week

---
+++ OPP/Enums/Day.cs
@@ -0,0 +1,12 @@
+namespace OPP.Enums;
+
+public enum Day
+{
+    Monday,    // Value: 0
+    Tuesday,   // Value: 1
+    Wednesday, // Value: 2
+    Thursday,  // Value: 3
+    Friday,    // Value: 4
+    Saturday,  // Value: 5
+    Sunday     // Value: 6
+}
