a8e54df0a9f8bf321b7050e584e6e01e48c7ff0d
dannyjy <danieljohn9295@gmail.com>
2026-01-17 23:02:32 +0200

    feat: add the fetching data console app to learn how to fetch data

---
+++ FetchingData/Program.cs
@@ -0,0 +1,29 @@
+﻿// using FetchingData.FetchTasks;
+// https://jsonplaceholder.typicode.com/todos/1
+
+namespace FetchingData
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            // await Todos.GetTodosAsync();
+            // await Users.GetUsersAsync();
+            GetSystemDetail();
+        }
+
+
+        public static void GetSystemDetail()
+        {
+            Console.WriteLine($"Machine Name: {Environment.MachineName}");
+            Console.WriteLine($"Machine Name: {Environment.CommandLine}");
+            Console.WriteLine($"OS Version: {Environment.OSVersion}");
+            Console.WriteLine($"OS Description: {System.Runtime.InteropServices.RuntimeInformation.OSDescription}");
+            Console.WriteLine($"Processor Count: {Environment.ProcessorCount}");
+            Console.WriteLine($"64-bit OS: {Environment.Is64BitOperatingSystem}");
+            Console.WriteLine($"User Name: {Environment.UserName}");
+            Console.WriteLine($"System Directory: {Environment.SystemDirectory}");
+            Console.WriteLine($"Working Set (Memory): {Environment.WorkingSet} bytes");
+        }
+    }
+}
