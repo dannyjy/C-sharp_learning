cfc32883e3506e45cc7e5c7ffbdde1b2ec5280c9
dannyjy <danieljohn9295@gmail.com>
2026-01-18 22:21:45 +0200

    fetch modify to fetch the todos as a lists of todos

--- FetchingData/FetchTasks/Todos.cs
+++ FetchingData/FetchTasks/Todos.cs
@@ -3,16 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Net.Http;
+using System.Security.Principal;
+using System.Net.Http.Json;
 
 namespace FetchingData
 {
+    public class Todo
+    {
+        private int _userId;
+        private int _id;
+        private string _title;
+        private bool _completed;
+
+        public int UserId { 
+            get { return _userId; }
+            set { _userId = value; } 
+        }
+        public int id { 
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string title {
+             get { return _title; } 
+             set { _title = value; }
+        }
+        public bool completed {
+             get { return _completed; } 
+             set { _completed = value; }
+        }
+    }
     public class Todos
     {
-        public static async Task GetTodosAsync()
+        public int Age { get; set; }
+        public static async Task<List<Todo>> GetTodosAsync()
         {
-            HttpClient httpClient = new HttpClient();
-            var reponse = await httpClient.GetStringAsync("https://jsonplaceholder.typicode.com/todos");
-            Console.WriteLine(reponse);
+            var client = new HttpClient();
+            return await client.GetFromJsonAsync<List<Todo>>("https://jsonplaceholder.typicode.com/todos") ?? new List<Todo>();
         }
     }
 }
